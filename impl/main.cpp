e7bc141ced1667deff59068843883e9dfd2e891c
Pranav <70764593+pranavbaburaj@users.noreply.github.com>
2021-04-08 17:35:50 +0530

    Formatted the document

--- impl/main.cpp
+++ impl/main.cpp
@@ -4,11 +4,35 @@
 
 using namespace std;
 
-vector<string> split(string str, const char *del) {
+/**
+ * Evokes a punch exception
+ * 
+ * @param message The error message
+ * @param type The type of error . Runtime, Argument etc.
+ * @param line_number The line at which the error occured
+ * 
+ * @returns void
+*/
+void throw_punch_error(string message, string type, int line_number)
+{
+    cout << "Found an Error:" << type << endl;
+    cout << message << "at line" << line_number;
+}
+
+/**
+ * Split a string by a delimiter
+ * 
+ * @param str The string to split
+ * @param del The del to split the string by
+ */
+vector<string> split(string str, const char *del)
+{
     vector<string> data;
     string current_string = "";
-    for(int x=0; x < str.length(); x++){
-        if(str[x] == *del){
+    for (int x = 0; x < str.length(); x++)
+    {
+        if (str[x] == *del)
+        {
             data.push_back(current_string);
             current_string = "";
             continue;
@@ -18,31 +42,77 @@ vector<string> split(string str, const char *del) {
     return data;
 }
 
-string remove_characters(string data, const char *character) {
+string remove_characters(string data, const char *character)
+{
     string current_string;
-    for(int index=0; index<data.length(); index++){
-        if(data[index] != *character){
+    for (int index = 0; index < data.length(); index++)
+    {
+        if (data[index] != *character)
+        {
             current_string += data[index];
         }
     }
     return current_string;
 }
 
-void run_punch_code(string data) {
-    vector<string> tokens = split(data, ";");
-    for(int tokenIndex=0; tokenIndex<tokens.size(); tokenIndex++){
-        vector<string> current_token = split(tokens[tokenIndex], " ");
-        
-    }
+void print(string print_text)
+{
+    cout << print_text << endl;
 }
 
-string find_filename(string filename) {
+bool run_punch_code(string data)
+{
+    vector<string> tokens = split(data, ";");
+    int lives = 100;
+    for (int tokenIndex = 0; tokenIndex < tokens.size(); tokenIndex++)
+    {
+        vector<string> current_token = split(tokens[tokenIndex], " ");
+        bool print_text = true;
+        if (current_token.size() == 2)
+        {
+            throw_punch_error("Found 0 commands", "RuntimeError", tokenIndex + 1);
+            return false;
+        }
+
+        string character = current_token[0];
+        if (character == "TrashTalkLn[str]")
+        {
+            lives -= 5;
+        }
+        else if (character == "TrashTalk[str]")
+        {
+            lives -= 5;
+        }
+        else if (character == "TrashTalk[int]" || character == "TrashTalkLn[int]")
+        {
+            lives -= 5;
+            print_text = false;
+            print(current_token[1]);
+        }
+        else if (character == "FaceSlap[var,int]")
+        {
+            lives -= 4;
+        }
+
+        if (lives > 0 && print_text)
+        {
+            print(remove_characters(
+                remove_characters(current_token[1], "\""),
+                "\\"));
+        }
+    }
+    return true;
+}
+
+string find_filename(string filename)
+{
     string file_text;
     string content;
     int line_count = 0;
     ifstream punch_source(filename);
 
-    while (getline(punch_source, file_text)) {
+    while (getline(punch_source, file_text))
+    {
         line_count += 1;
         content += file_text;
     }
@@ -51,19 +121,17 @@ string find_filename(string filename) {
     return content;
 }
 
-void throw_punch_error(string message, string type, int line_number) {
-    cout << "Found an Error:" << type << endl;
-    cout << message << "at line" << line_number;
-}
-
-int main(int argc, char *argv[]){
-    if (argc != 2) {
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
         throw_punch_error(
             "Missing required parameter - filename",
             "ArgumentError",
-            0
-        );
-    } else {
+            0);
+    }
+    else
+    {
         string file_content = find_filename(argv[1]);
         run_punch_code(file_content);
     }
